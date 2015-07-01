8c98fb029794c24414c1672fa16177d05b8be963
Mike Hingley <mike.hingley@opengi.co.uk>
2015-07-01 08:19:23 +0100

    Adding .NET remote Controller Application

---
+++ Java Remote Controller/Java Remote Controller/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Java_Remote_Controller
+{
+   public  class Class1 : ApiController
+    {
+       public string ReturnHelloWorld()
+       {
+           return "Hello World (From .NET)";
+       }
+    }
+}
---
+++ Java Remote Controller/Java Remote Controller/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Web.Http;
+using System.Web.Http.SelfHost;    
+
+namespace Java_Remote_Controller
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            var config = new HttpSelfHostConfiguration("http://localhost:8080");
+
+            config.Routes.MapHttpRoute("API Default", "api/{controller}/{id}",new { id = RouteParameter.Optional });
+            using (HttpSelfHostServer server = new HttpSelfHostServer(config)) 
+            {
+                server.OpenAsync().Wait();
+
+            }
+
+
+
+
+
+            rc = new ServiceReference1.RemoteControlServiceClient();
+            rc.ClientCredentials.UserName.UserName = "admin";
+            rc.ClientCredentials.UserName.Password = "password";
+            ws = new WebSocket4Net.WebSocket("ws://localhost:1970/RemoteService/RemoteControl");
+            ws.MessageReceived +=ws_MessageReceived;
+        }
+
+        void ws_MessageReceived(object sender, WebSocket4Net.MessageReceivedEventArgs e)
+        {
+            textBox1.Text = textBox1.Text + e.Message + "\n";
+        }
+        private WebSocket4Net.WebSocket ws;
+
+        private ServiceReference1 .RemoteControlServiceClient rc = new ServiceReference1.RemoteControlServiceClient();
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            var s = rc.ScreenShot("MainForm");
+            using (var ms = new System.IO.MemoryStream(s)) {
+                pictureBox1.Image = Image.FromStream(ms);
+ 
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            rc.PressButton();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ws.Send("hello");
+          
+        }
+    }
+}
